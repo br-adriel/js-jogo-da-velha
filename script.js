98c4517ec3f67c7e792324dc9151a97c3fc856b5
Adriel <adriel.fariasan@gmail.com>
2022-01-10 20:24:31 -0300

    Implementa tabuleiroHtml parcialmente

--- script.js
+++ script.js
@@ -18,14 +18,18 @@ const tabuleiro = (() => {
       ) {
         return _tabuleiro[0][0];
       }
-    } else if (_tabuleiro[1][0] !== "") {
+    }
+
+    if (_tabuleiro[1][0] !== "") {
       if (
         _tabuleiro[1][1] === _tabuleiro[1][0] &&
         _tabuleiro[1][2] === _tabuleiro[1][0]
       ) {
         return _tabuleiro[1][0];
       }
-    } else if (_tabuleiro[2][0] !== "") {
+    }
+
+    if (_tabuleiro[2][0] !== "") {
       if (
         _tabuleiro[2][1] === _tabuleiro[2][0] &&
         _tabuleiro[2][2] === _tabuleiro[2][0]
@@ -42,14 +46,18 @@ const tabuleiro = (() => {
       ) {
         return _tabuleiro[0][0];
       }
-    } else if (_tabuleiro[0][1] !== "") {
+    }
+
+    if (_tabuleiro[0][1] !== "") {
       if (
         _tabuleiro[1][1] === _tabuleiro[0][1] &&
         _tabuleiro[2][1] === _tabuleiro[0][1]
       ) {
         return _tabuleiro[0][1];
       }
-    } else if (_tabuleiro[0][2] !== "") {
+    }
+
+    if (_tabuleiro[0][2] !== "") {
       if (
         _tabuleiro[1][2] === _tabuleiro[0][2] &&
         _tabuleiro[2][2] === _tabuleiro[0][2]
@@ -66,7 +74,9 @@ const tabuleiro = (() => {
       ) {
         return _tabuleiro[0][0];
       }
-    } else if (_tabuleiro[0][2] !== "") {
+    }
+
+    if (_tabuleiro[0][2] !== "") {
       if (
         _tabuleiro[1][1] === _tabuleiro[0][2] &&
         _tabuleiro[2][0] === _tabuleiro[0][2]
@@ -79,11 +89,11 @@ const tabuleiro = (() => {
     return -1;
   };
   const limpar = () => {
-    _tabuleiro.forEach((linha) => {
-      linha.forEach((coluna) => {
-        coluna = "";
-      });
-    });
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        _tabuleiro[i][j] = "";
+      }
+    }
   };
   const jogar = (linha, coluna, simbolo) => {
     if (_tabuleiro[linha][coluna] === "") {
@@ -109,6 +119,11 @@ const Jogador = (nome, simbolo, cor) => {
   const verSimbolo = () => _simbolo;
   const alterarCor = (novaCor) => {
     _cor = novaCor;
+    if (_simbolo == "X") {
+      placarHtml.definirCorX(_cor);
+    } else {
+      placarHtml.definirCorO(_cor);
+    }
   };
   const verCor = () => _cor;
   const novaVitoria = () => {
@@ -158,3 +173,86 @@ const placarHtml = (() => {
     definirCorO,
   };
 })();
+
+// elementos html do tabuleiro
+const tabuleiroHtml = (() => {
+  // botoes do tabuleiro
+  let _vezDoX = true;
+  const _celulas = document.getElementsByClassName("tab");
+
+  for (let i = 0; i < _celulas.length; i++) {
+    _celulas[i].addEventListener("click", (e) =>
+      _clicarCelula(e.currentTarget)
+    );
+  }
+
+  const _desabilitarCelulas = () => {
+    for (let i = 0; i < _celulas.length; i++) {
+      _celulas[i].disabled = true;
+    }
+  };
+
+  const _clicarCelula = (botao) => {
+    if (!botao.classList.contains("usado")) {
+      const icone = document.createElement("i");
+      const linhaJogada = parseInt(botao.id.split("-")[1]);
+      const colunaJogada = parseInt(botao.id.split("-")[2]);
+
+      if (_vezDoX) {
+        icone.classList.add("fas", "fa-times");
+        icone.style.color = JOGADOR_X.verCor();
+
+        botao.appendChild(icone);
+        botao.classList.add("usado");
+
+        const ganhou = tabuleiro.jogar(linhaJogada, colunaJogada, "X");
+        if (ganhou != -1) {
+          console.log("X GANHOU");
+
+          _desabilitarCelulas();
+          JOGADOR_X.novaVitoria();
+          placarHtml.definirPontosX(JOGADOR_X.verVitorias());
+        }
+      } else {
+        icone.classList.add("far", "fa-circle");
+        icone.style.color = JOGADOR_O.verCor();
+
+        botao.appendChild(icone);
+        botao.classList.add("usado");
+
+        const ganhou = tabuleiro.jogar(linhaJogada, colunaJogada, "O");
+        if (ganhou != -1) {
+          console.log("O GANHOU");
+
+          _desabilitarCelulas();
+          JOGADOR_O.novaVitoria();
+          placarHtml.definirPontosO(JOGADOR_O.verVitorias());
+        }
+      }
+      _vezDoX = !_vezDoX;
+    }
+  };
+
+  const limpar = () => {
+    for (let i = 0; i < _celulas.length; i++) {
+      _celulas[i].innerText = "";
+      _celulas[i].classList.remove("usado");
+      _celulas[i].disabled = false;
+    }
+
+    tabuleiro.limpar();
+  };
+
+  return { limpar };
+})();
+
+const JOGADOR_X = Jogador("Adriel", "X", COR_INICIAL_X);
+const JOGADOR_O = Jogador("Jogador 2", "O", COR_INICIAL_O);
+
+placarHtml.definirNomeX(JOGADOR_X.verNome());
+placarHtml.definirCorX(JOGADOR_X.verCor());
+placarHtml.definirPontosX(JOGADOR_X.verVitorias());
+
+placarHtml.definirNomeO(JOGADOR_O.verNome());
+placarHtml.definirCorO(JOGADOR_O.verCor());
+placarHtml.definirPontosO(JOGADOR_O.verVitorias());
